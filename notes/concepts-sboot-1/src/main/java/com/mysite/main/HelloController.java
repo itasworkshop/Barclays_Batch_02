cd21284258c8e9667b3849252af76c46f1820efe
itasworkshop <itasworkshop@gmail.com>
2022-08-17 09:23:50 +0530

    17 Aug 1

---
+++ notes/concepts-sboot-1/src/main/java/com/mysite/main/HelloController.java
@@ -0,0 +1,29 @@
+package com.mysite.main;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String helloSpring() {
+		return "Hello from my first spring controller";
+	}
+	
+	@RequestMapping("/users")
+	@ResponseBody
+	String helloUsers() {
+		return "Hello from my first users controller.";
+	}
+	
+	@RequestMapping("/users/{name}")
+	@ResponseBody
+	String helloUserBasedOnName(@PathVariable String name) {
+		return "Hello from my first user controller your name is "+name;
+	}
+
+}
